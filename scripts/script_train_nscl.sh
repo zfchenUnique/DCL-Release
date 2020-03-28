cd0ba41f9999a5bdb1cc0ecfe1f7b921e3b422b4
zfchen <zfchen@cs.hku.hk>
2020-03-28 23:51:21 +0800

    normalized boxes

--- scripts/script_train_nscl.sh
+++ scripts/script_train_nscl.sh
@@ -2,6 +2,6 @@ GPU_ID=$1
 jac-crun ${GPU_ID} scripts/trainval_tube.py --desc clevrer/desc_nscl_derender_clevrer.py\
     --training-target derender --curriculum all\
     --dataset clevrer --data-dir ../clevrer\
-    --batch-size 16 --epoch 100 --validation-interval 5\
+    --batch-size 8 --epoch 100 --validation-interval 5\
     --save-interval 5 --data-split 0.95 --data-workers 1 \
     --normalized_boxes 1
