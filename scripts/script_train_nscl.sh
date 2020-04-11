3cef7f1d4425bf51d7279ae3716eabb25508d140
zfchen <zfchen@cs.hku.hk>
2020-04-11 21:10:43 +0800

    train script

--- scripts/script_train_nscl.sh
+++ scripts/script_train_nscl.sh
@@ -5,15 +5,16 @@ jac-crun ${GPU_ID} scripts/trainval_tube.py --desc clevrer/desc_nscl_derender_cl
     --batch-size 4 --epoch 100 --validation-interval 5\
     --save-interval 5 --data-split 0.95 --data-workers 2 \
     --normalized_boxes 1 --frm_img_num 6 --even_smp_flag 1\
-    --rel_box_flag 0 --acc-grad 4 --dynamic_ftr_flag 1 --version v5_gt_new_col_diff_mov_new_mask\
+    --rel_box_flag 0 --acc-grad 4 --dynamic_ftr_flag 1 --version v5_gt_new_col_diff_mov_new_mask_gau_std1\
     --scene_supervision_flag 1 \
     --tube_prp_path ../clevrer/tubeProposalsGt \
     --scene_add_supervision 0 \
     --scene_supervision_weight 0.5 \
     --box_iou_for_collision_flag 1 \
     --diff_for_moving_stationary_flag 1 \
-    --resume dumps/remote_models/new_collision_15.pth \
-    --new_mask_out_value_flag 1
+    --new_mask_out_value_flag 1 \
+    --apply_gaussian_smooth_flag 1
+    #--resume dumps/remote_models/new_collision_15.pth \
     #--resume dumps/clevrer/desc_nscl_derender_clevrer/derender_norm_box_even_smp6_col_box_ftr_v5_gt_thre_in_out_new/checkpoints/epoch_5.pth \
     #--resume dumps/clevrer/desc_nscl_derender_clevrer/derender_norm_box_even_smp6_col_box_ftr_v5_gt_sym_col_scene_super_balance_thre_in_out_new/checkpoints/epoch_5.pth
     #--load dumps/remote_models/scene_super_25.pth
