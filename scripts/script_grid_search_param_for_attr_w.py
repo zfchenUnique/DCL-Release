074cd6452d995d325337250392ea6bbb2018fc4c
Zhenfang Chen <u3004417@gpu2gate1.gpufarm2.cs.hku.hk>
2020-07-19 13:09:14 +0800

    new pretrain model

--- scripts/script_grid_search_param_for_attr_w.py
+++ scripts/script_grid_search_param_for_attr_w.py
@@ -6,10 +6,13 @@ def main():
     #for attr_w in np.arange(0.5,  3.5, 0.5):
     #for attr_w in np.arange(0.1,  1, 0.1):
     for match_thre in np.arange(0.1,  1, 0.1):
-        attr_w = 0.5 #float(attr_w)
+    #for attr_w in np.arange(0.1,  1, 0.1):
+        #attr_w = float(attr_w)
+        attr_w = 0.5
         match_thre = float(match_thre)
+        #match_thre = 0.7
         #pdb.set_trace()
-        cmd_text= 'python scripts/script_gen_tube_proporals.py --tube_folder_path ../clevrer/tubeProposalsAttrV2 --extract_att_path dumps/clevrer/tmpProposalsAttr --attr_w %f  --match_thre %f' % (attr_w, match_thre)
+        cmd_text= 'python scripts/script_gen_tube_proporals.py --tube_folder_path ../clevrer/tubeProposalsAttrTestRefine --extract_att_path dumps/clevrer/tmpProposalsAttrTest_ep10Refine --attr_w %f  --match_thre %f' % (attr_w, match_thre)
         os.system(cmd_text)
 
 if __name__=='__main__':
