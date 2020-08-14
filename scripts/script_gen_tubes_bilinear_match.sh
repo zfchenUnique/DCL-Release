87f25a1250292cc2de10a06c1a9f757ac6dd234d
Zhenfang Chen <u3004417@gpu2gate1.gpufarm2.cs.hku.hk>
2020-08-14 12:28:05 +0800

    using bilinear matching to solve the tracking problem

---
+++ scripts/script_gen_tubes_bilinear_match.sh
@@ -0,0 +1,5 @@
+python scripts/script_gen_tube_proporals.py --tube_folder_path ../clevrer/tubeProposalsAttrMatch \
+    --extract_att_path dumps/clevrer/tmpProposalsAttr \
+    --attr_w 0.4 \
+    --use_attr_flag 1 \
+    --match_thre 0.7
