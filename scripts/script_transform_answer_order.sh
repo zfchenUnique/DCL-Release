84de0a0b6f717a3eb3c02f042d3d81f1bcd82a6c
Zhenfang <zfchen@cs.hku.hk>
2020-12-30 22:44:18 +0800

    Delete script_transform_answer_order.sh
    update on scripts

--- scripts/script_transform_answer_order.sh
+++
@@ -1,4 +0,0 @@
-python ./scripts/script_rerange_choices.py \
-    --in_path dumps/test_ep7_release.json \
-    --raw_ques_path data/raw_data/raw_questions/multiple_choice_questions.json \
-    --release_ques_path ../clevrer/questions/test.json \
